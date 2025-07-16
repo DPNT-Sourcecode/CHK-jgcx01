7fa770ae330c894a6c2359f32e6116b2d33dc1c5
sbx_user1051 <sbx_user1051@169.254.29.63>
2025-07-16 12:11:55 +0000

    Wed Jul 16 12:11:55 UTC 2025

--- lib/solutions/SUM/sum_solution.py
+++ lib/solutions/SUM/sum_solution.py
@@ -2,5 +2,13 @@
 class SumSolution:
     
     def compute(self, x, y):
+        #Checking that both x and y are both postive intergers between 0 and 100 
+        if (not isinstance(x, (int,float)) or not isinstance(y, (int,float))):
+            raise TypeError("Both x and y should be postive integers")
+
+        if not (0 <= x <= 100) or not (0 <= y <= 100):
+            raise TypeError("Both x and y should be between 0 and 100")
+        
         return x + y
+
 
