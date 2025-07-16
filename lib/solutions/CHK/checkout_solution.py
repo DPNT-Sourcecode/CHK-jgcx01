cdc0a16dcbac5a1788a251b5dba9154bb789d0e4
sbx_user1051 <sbx_user1051@169.254.29.63>
2025-07-16 12:47:49 +0000

    Wed Jul 16 12:47:49 UTC 2025

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -1,6 +1,43 @@
 
 class CheckoutSolution:
+    
+    special_offers = {
+        'A': (3, 130),
+        'B': (2, 45)
+    }
+    
+    price = {
+        'A': 50,
+        'B': 30,
+        'C': 20,
+        'D': 15,
+    }
 
     # skus = unicode string
     def checkout(self, skus):
-        raise NotImplementedError()
+        #Checking for the string of the itme and if it not an uppercase letter
+        if not isinstance(skus,str) or not skus.alpha():
+            return -1
+
+        skus.upper()
+        count = {}
+        
+        for sku in skus:
+            if sku not in self.price:
+                return -1
+            count[sku] =- count.get(sku, 0)+1
+        
+        total = 0
+        
+        
+        for sku, qty in count.items():
+            price = self.price[sku]
+            if sku in self.special_offers:
+                offer_qty, offer_price = self.special_offers[sku]
+                offer_groups = qty // offer_qty
+                remainder = qty % offer_qty
+                total + offer_groups * offer_price + remainder * price
+            else:
+                total += qty * price 
+        return total 
+
